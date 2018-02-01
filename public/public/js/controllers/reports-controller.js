20a6d71401df0c6ea89fdb049fd99e857a47caea
Augusto Moguel <augusto.moguel@gmail.com>
2018-02-01 12:54:26 -0600

    Pagina report list

--- public/public/js/controllers/reports-controller.js
+++ public/public/js/controllers/reports-controller.js
@@ -5,13 +5,25 @@
 
 		$scope.isMobileDevice = $mdMedia('xs');
 		$scope.reports = [];
+		$scope.report="";
 
 		$scope.init = function(){
 			$scope.promise = ReportService.getListReport();
 	    	
 	    	$scope.promise.then(
 		          function(result) { 
-		          	  console.log("ReportService.getListReport",result);
+		          	var firts=true;
+		          	angular.forEach(result.data.d.results, function(r, key) {
+		          		var obj= angular.fromJson(r.Json);
+		          		$scope.reports.push(obj);
+		          		if(firts)
+		          		{
+		          			$scope.report = obj.REPORT_NAME;
+		          			firts=false;
+		          		}
+					 
+					});		  
+					console.log($scope.reports);        	  
 		     	  },
 		          function(errorPayload) {
 		              console.log('failureReportService.getListReport', errorPayload);
